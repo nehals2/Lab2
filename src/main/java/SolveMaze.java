a55b9ebc6c760207d0a03ab9085e421e97531bc1
NehalSingh <nehals2@illinois.edu>
2018-09-12 20:31:11 -0500

    fixed infinite loop.

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -33,9 +33,17 @@ public class SolveMaze {
          * You should be able to solve a 10 x 10 maze in (far fewer than) 1000 steps.
          * Feel free to adjust this number if you experiment with other mazes.
          */
+        // Implement your maze solving algorithm here
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                maze.turnLeft();
+                while (!maze.canMove()) {
+                    maze.turnRight();
+                }
+                maze.move(); }
         }
+
+
 
         if (maze.isFinished()) {
             System.out.println("You solved the maze!");
@@ -43,4 +51,9 @@ public class SolveMaze {
             System.out.println("Try again!");
         }
     }
+
 }
+
+
+
+
